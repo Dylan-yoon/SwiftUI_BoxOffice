1a29915db4d5b240cf16f650bd643103b7313a33
dylan-yoon <d.dylany21@gmail.com>
2024-03-06 15:42:52 +0900

    build: ProjectSetting

---
+++ SwiftUI_BoxOffice/SwiftUI_BoxOffice/SwiftUI_BoxOfficeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_BoxOfficeApp.swift
+//  SwiftUI_BoxOffice
+//
+//  Created by Dylan_Y on 3/6/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_BoxOfficeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
